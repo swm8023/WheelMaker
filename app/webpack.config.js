62667fdd4b8ef0e5115dc4c712acd4b0ef4a432b
swm8023 <swm8023@gmail.com>
2026-03-27 08:28:20 +0800

    fix(app): resolve async-storage web module resolution

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -18,6 +18,12 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
         test: /\.[jt]sx?$/,
         exclude: modulePath =>
           /node_modules/.test(modulePath) &&
