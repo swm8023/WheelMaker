f662c028d4cecf5907dd33980fe4fdda21d88266
swm8023 <swm8023@gmail.com>
2026-03-28 15:39:45 +0800

    Rename app runtime name to WheelMaker

--- app/android/app/src/main/java/com/wheelmakerrn/MainActivity.kt
+++ app/android/app/src/main/java/com/wheelmakerrn/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "WheelMakerRN"
+  override fun getMainComponentName(): String = "WheelMaker"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
